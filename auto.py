b60d1dbb3baa5a5016ca7f160f88ad861ce160b9
zhang-bai <zhangbai2316@mails.jlu.edu.cn>
2022-05-20 18:40:53 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -2,9 +2,9 @@ import schedule, random, time
 from users import *
 from main import check
 
-minute = random.randint(1, 10)
+minute = random.randint(10, 30)
 CHECK_SCHEDULES = [
-    (7, minute),
+    (8, minute),
     (21, minute)
 ]
 
